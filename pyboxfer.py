60f24aa45bc1368951315fc98ff3032f22b140ed
Jordan Gumm <jngumm@gmail.com>
2018-02-23 05:53:56 -0500

    implemented initial download command

--- pyboxfer.py
+++ pyboxfer.py
@@ -1,6 +1,7 @@
 from boxsdk import OAuth2, Client
 import click
 import yaml
+import os
 
 
 class Auth(object):
@@ -25,18 +26,63 @@ def cli(ctx, client_id, client_secret, access_token, config_fp):
 
 
 @cli.command()
-@click.argument('path', required=False)
+@click.argument('dirpath', required=False)
+@click.option('--folder_id', '-i', required=False)
+@click.pass_context
 @click.pass_obj
-def ls(auth, path):
+def lsdir(auth, ctx, dirpath, folder_id):
     """ Lists items in directory path """
-    root_folder = auth.client.folder(folder_id='0').get()
-    for item in auth.client.folder(folder_id='0').get_items(limit=100, offset=0):
-        if type(item) == type(root_folder):
+    folder_id = folder_id or '0'
+
+    folder = auth.client.folder(folder_id=folder_id).get()
+    print folder['name']
+
+    next_folder = None
+    if dirpath: next_folder = dirpath.split('/')[0]
+        
+    for item in auth.client.folder(folder_id=folder_id).get_items(limit=1000, offset=0):
+        if type(item) == type(folder):
             item = auth.client.folder(folder_id=item['id']).get()
+            if item['name'] == next_folder:
+                ctx.invoke(lsdir, dirpath='/'.join(dirpath.split('/')[1:]), folder_id=item['id'])
         else:
             item = auth.client.file(file_id=item['id']).get()
-        print '{}\t{}\t{}'.format(item['owned_by']['login'], item['size'], item['name']).expandtabs(25)
-    
+        if not next_folder: # only print if at end of given path
+            print '{}\t{}\t{}'.format(item['owned_by']['login'], item['size'], item['name']).expandtabs(25)
+
+
+@cli.command('download')
+@click.argument('path', required=True)
+@click.option('--folder_id', '-i', required=False)
+@click.option('--output', '-o', default='.')
+@click.pass_context
+@click.pass_obj
+def download(auth, ctx, path, folder_id, output):
+    if not os.path.exists(output): os.makedirs(output)
+    folder_id = folder_id or '0'
+
+    folder = auth.client.folder(folder_id=folder_id).get()
+
+    next_folder = None
+    if path: next_folder = path.split('/')[0]
+    print '{} -> {}'.format(folder['name'], next_folder)
+    for item in auth.client.folder(folder_id=folder_id).get_items(limit=1000, offset=0):
+        if type(item) == type(folder):
+            item = auth.client.folder(folder_id=item['id']).get()
+            if item['name'] == next_folder:
+                ctx.invoke(download, path='/'.join(path.split('/')[1:]), folder_id=item['id'], output=output)
+        else:
+            item = auth.client.file(file_id=item['id']).get()
+            if item['name'] == next_folder and len(path.split('/')) == 1:
+                print 'downloading {}'.format(item['name'])
+                with open(os.path.join(output, item['name']), 'w+') as output_file:
+                    item.download_to(output_file)
+                return
+        if not next_folder: # only print if at end of given path
+            print 'downloading {}'.format(item['name'])
+            with open(os.path.join(output, item['name']), 'w+') as output_file:
+                    item.download_to(output_file)
+
 
 if __name__ == "__main__":
     cli()
